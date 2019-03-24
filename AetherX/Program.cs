5536d1b1734ed44df5da0e697d4aba3e02116bfc
Dhyan <dhyanpp@gmail.com>
2019-03-23 20:03:58 -0500

    Initial commit

---
+++ AetherX/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace AetherX {
+    class Program {
+        private static DiscordClient discord;
+
+        static void Main(string[] args) {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args) {
+
+            // Instantiates Discord Client
+            discord = new DiscordClient(new DiscordConfiguration {
+                Token = "NTU5MTQ1NjA0MTMxODQ4MjAy.D3hKzg.y4YDKboazuT9JusmrUMk1w4Nams",
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e => {
+                if (e.Message.Content.ToLower().StartsWith("ping")) {
+                    await e.Message.RespondAsync("Pong!");
+                    Console.WriteLine("Ponging");
+                }
+            };
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+}

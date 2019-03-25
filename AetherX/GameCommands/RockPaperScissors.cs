ad17851c6a4e8a7ba5543c498527f5f06f62e47f
Dhyan <dhyanpp@gmail.com>
2019-03-25 01:37:13 -0500

    Implemented Embed Builder for RPS
    Signed-off-by: Dhyan <dhyanpp@gmail.com>

--- AetherX/GameCommands/RockPaperScissors.cs
+++ AetherX/GameCommands/RockPaperScissors.cs
@@ -2,14 +2,15 @@ using System.Threading.Tasks;
 using AetherX.HelperFiles;
 using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace AetherX.GameCommands {
     public class RockPaperScissors {
 
-        
         [Command("rps")]
         public async Task RPS(CommandContext ctx, string choice) {
-            int computer = ThreadLocalRandom.Instance.Next(3); // https://codeblog.jonskeet.uk/2009/11/04/revisiting-randomness/
+            int computer =
+                ThreadLocalRandom.Instance.Next(3); // https://codeblog.jonskeet.uk/2009/11/04/revisiting-randomness/
             int player;
             switch (choice.ToLower()) {
                 case "rock":
@@ -22,35 +23,84 @@ namespace AetherX.GameCommands {
                     player = 2;
                     break;
                 default:
-                    await ctx.RespondAsync($"Invalid Response {choice}!");
+                    await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                        .WithColor(DiscordColor.Red)
+                        .WithTitle("Invalid Response")
+                        .WithDescription($"\"{choice}\" is not a valid Ressponse")
+                        .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
                     return;
             }
 
             if (player == computer) {
-                await ctx.RespondAsync($"It's a Tie! You both chose {choice}!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Gold)
+                    .WithTitle("It's a Tie!")
+                    .AddField("Computer", choice, true)
+                    .AddField("Player", choice, true)
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
                 return;
             }
 
             if (player == 0 && computer == 1) {
-                await ctx.RespondAsync("You Chose Rock. Computer Chose Paper.\n" +
-                                       "You Lose!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Red)
+                    .WithTitle("You Lose!")
+                    .AddField("Computer", "paper", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Paper covers Rock")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else if (player == 0 && computer == 2) {
-                await ctx.RespondAsync("You Chose Rock. Computer Chose Scissors.\n" +
-                                       "You Win!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Green)
+                    .WithTitle("You Win!")
+                    .AddField("Computer", "scissors", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Rock smashes Scissors")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else if (player == 1 && computer == 0) {
-                await ctx.RespondAsync("You Chose Paper. Computer Chose Rock.\n" +
-                                       "You Win!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Green)
+                    .WithTitle("You Win!")
+                    .AddField("Computer", "rock", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Paper covers Rock")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else if (player == 1 && computer == 2) {
-                await ctx.RespondAsync("You Chose Paper. Computer Chose Scissors.\n" +
-                                       "You Lose!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Red)
+                    .WithTitle("You Lose!")
+                    .AddField("Computer", "scissors", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Scissors cuts Paper")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else if (player == 2 && computer == 0) {
-                await ctx.RespondAsync("You Chose Scissors. Computer Chose Rock.\n" +
-                                       "You Lose!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Red)
+                    .WithTitle("You Lose!")
+                    .AddField("Computer", "scissors", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Rock smashes Scissors")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else if (player == 2 && computer == 1) {
-                await ctx.RespondAsync("You Chose Scissors. Computer Chose Paper.\n" +
-                                       "You Win!");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Green)
+                    .WithTitle("You Win!")
+                    .AddField("Computer", "paper", true)
+                    .AddField("Player", choice, true)
+                    .WithFooter("Scissors cuts Paper")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
+
             } else {
-                await ctx.RespondAsync("Something went wrong! Sorry :(");
+                await ctx.RespondAsync(embed: new DiscordEmbedBuilder()
+                    .WithColor(DiscordColor.Red)
+                    .WithTitle("ERROR")
+                    .WithDescription("Something went wrong! Sorry :(")
+                    .WithAuthor(ctx.User.Username, null, ctx.User.AvatarUrl));
             }
         }
     }
